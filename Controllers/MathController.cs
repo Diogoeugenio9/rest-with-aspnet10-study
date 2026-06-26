65d0e3ec95dca08da7f0c36eed2a0d89bf796088
diogoeugenio920@gmail.com <diogoeugenio920@gmail.com>
2026-06-25 22:44:36 -0300

    feat: adiciona endpoints de media e raiz quadrada

--- Controllers/MathController.cs
+++ Controllers/MathController.cs
@@ -17,6 +17,29 @@ namespace RestWithASPNET10Study.Controllers
             return BadRequest("Invalid Input!");
         }
 
+        [HttpGet("media/{firstNumber}/{secondNumber}")]
+        public IActionResult Media(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var media = (ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber)) / 2;
+                return Ok(media);
+            }
+            return BadRequest("Invalid Input");
+
+        }
+
+        [HttpGet("square/{number}")]
+        public IActionResult Square(string number)
+        {
+            if (IsNumeric(number))
+            {
+                var sqrt = Math.Sqrt((double)ConvertToDecimal(number));
+                return Ok(sqrt);
+            }
+            return BadRequest("Invalid Input");
+        }
+
         private decimal ConvertToDecimal(string strNumber)
         {
             decimal decimalValue;
